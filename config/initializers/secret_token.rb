90384935e301022538d5da8454a244a824ae784b
Sanjay Bakshi <sanjay_bakshi@yahoo.com>
2012-10-29 22:07:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FBCPService::Application.config.secret_token = 'c320ed65d08fe9de4c217827b819d3e6dc869b615ee30dbe2251c4eee283699682fc5549e865c04d17ab49b16facb574cf1f63d5b3564850f63278766cd4e365'
