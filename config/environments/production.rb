7ecb663fa2b3cd4050f1a892b84d2814e0daf71b
Sanjay Bakshi <sanjay_bakshi@yahoo.com>
2012-11-23 16:43:41 -0800

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,9 @@ FBCPService::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Facebook setting
-#  APP_ID="166864856786067"
-#  SITE_URL="http://blooming-oasis-4471.herokuapp.com/"
-#  APP_SECRET="1b681b2579723f696c48775a6d8a302a"
-
+  APP_ID="166864856786067"
+  APP_SECRET="1b681b2579723f696c48775a6d8a302a"
+  SITE_URL="http://blooming-oasis-4471.herokuapp.com/"
   XSITEURL="http://blooming-oasis-4471.herokuapp.com/"
 
 end
